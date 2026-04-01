a78372c442301e14006caf85aff89f796a5a4adb
abduxla <abdullahjr711@gmail.com>
2026-04-01 21:38:09 +0530

    Complete setup with working discovery

--- src/main/java/com/abdullah/smartcampus/Main.java
+++ src/main/java/com/abdullah/smartcampus/Main.java
@@ -8,7 +8,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class Main {
 
-    public static final String BASE_URI = "http://localhost:8080/api/";
+    public static final String BASE_URI = "http://localhost:9095/api/";
 
     public static HttpServer startServer() {
         final ResourceConfig config = ResourceConfig
@@ -18,11 +18,11 @@ public class Main {
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         HttpServer server = startServer();
         System.out.println("Server started at " + BASE_URI);
-        System.out.println("Try: http://localhost:8080/api/v1");
-
-        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdownNow));
+        System.out.println("Open this URL: " + BASE_URI + "v1");
+        Thread.sleep(Long.MAX_VALUE);
+        server.shutdownNow();
     }
 }
